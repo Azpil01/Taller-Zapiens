161bdc10da2a428be6967feb2308f2a47bbd48a5
Azpil <eazpil01@gmail.com>
2025-12-15 22:50:59 -0600

    Initial commit complete v1

---
+++ index.js
@@ -0,0 +1,15 @@
+import express from "express";
+import ejs from "ejs";
+
+const port = 3000;
+const app = new express();
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    res.render("index.ejs");
+})
+
+app.listen(port, () => {
+    console.log(`All good from port ${port} ` );
+})
