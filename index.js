e103abc39d50f92495883fb35d5aa129d89b126f
Azpil <eazpil01@gmail.com>
2026-02-24 21:07:25 -0600

    [Título]: Feat. Add the form
    [Descripción]: Adds the form and the footer section.
    
    [Tareas/Issues]:

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 import express from "express";
-import ejs from "ejs";
 import ms from "ms";
+import nodemailer from "nodemailer";
+import bodyParser from "body-parser";
+import  'dotenv/config';
+
+
+
+const userGmail= process.env.userGmail;
+const appPasword = process.env.appPasword;
+
 
 
 const port = 3000;
@@ -10,18 +18,59 @@ const version = Date.now();
 
 app.locals.version = version;
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
 app.use(express.static("public", {
     maxAge: ms("1d"),
     etag: true
 }));
 
+
+
+
+
+
+
 app.get("/", (req, res) => {
     res.render("index.ejs");
+    
 })
 
-app.get("/us", (req, res) => {
-    console.log("It is the us sections");
+app.post("/sendMessage", (req, res) => {
+    
+    const nombre = req.body.nombre;
+    const subject = req.body.mensaje;
+    const correo = req.body.correo;
+
+    const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+        user: userGmail,
+        pass: appPasword,
+    } 
+});
+
+const mailOptions = {
+    from: correo,
+    to: userGmail,
+    subject: `Mail from ${nombre}`,
+    text: subject
+}
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log("Email sent: " + info.response);
+        }
+    })
+
+    res.redirect("/");
 })
+
+
 
 app.get("/contact", (req, res) => {
     console.log("The contact info")
